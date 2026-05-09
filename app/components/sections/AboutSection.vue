178cd1371ff8289d6ad530969d01b59bfe58c29b
Miguel Angel Escamilla <rodrigo.tejero@thewebchi.mp>
2026-05-08 20:38:11 -0600

    feat: add AboutSection with skill groups and photo placeholder

---
+++ app/components/sections/AboutSection.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import SkillGroup from '../ui/SkillGroup.vue'
+import { skillGroups } from '../../content/skills'
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <section id="about" class="about">
+    <div class="container py-5">
+      <div class="row mb-4 g-4">
+        <div class="col-md-4">
+          <span class="section-label">{{ t('about.label') }}</span>
+        </div>
+        <div class="col-md-8">
+          <p class="about__intro">{{ t('about.intro') }}</p>
+        </div>
+      </div>
+
+      <div class="row g-4 align-items-start">
+        <div class="col-lg-7 d-flex flex-column gap-3">
+          <SkillGroup
+            v-for="group in skillGroups"
+            :key="group.id"
+            :title-key="`about.skillsTitle.${group.id}`"
+            :items="group.items"
+          />
+        </div>
+
+        <div class="col-lg-5">
+          <img
+            src="/images/photo-placeholder.svg"
+            :alt="t('about.photoAlt')"
+            class="about__photo img-fluid rounded-4"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.about {
+  padding-block: 4rem;
+
+  &__intro {
+    font-size: 1.125rem;
+    color: $text-primary;
+  }
+
+  &__photo {
+    filter: grayscale(100%);
+    width: 100%;
+    border: 1px solid $border-subtle;
+  }
+}
+</style>
