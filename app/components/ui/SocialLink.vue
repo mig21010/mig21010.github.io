a6bff6da704b2a8c00c2a069941eabf3d8425266
Miguel Angel Escamilla <rodrigo.tejero@thewebchi.mp>
2026-05-08 20:29:30 -0600

    feat: add SocialLink component

---
+++ app/components/ui/SocialLink.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  icon: string
+  url: string
+  label: string
+}>()
+</script>
+
+<template>
+  <a
+    class="social-link"
+    :href="url"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <i :class="['bi', icon]" aria-hidden="true" />
+    <span>{{ label }}</span>
+  </a>
+</template>
